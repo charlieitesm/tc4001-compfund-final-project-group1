78a3aa0f7f3b85a448a7293b20def8433d11d74d
Byron <cibermania5@gmail.com>
2019-11-10 23:17:53 -0600

    saving working changes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 
 from automata.state_machine import Automaton
-from utils.file_utils import deserialize_automaton
+from utils.file_utils import deserialize_automaton, serialize_automaton, save_str_to_file
 
 
 class MyTestCase(unittest.TestCase):
@@ -15,6 +15,32 @@ class MyTestCase(unittest.TestCase):
     def test_good_file(self) -> None:
         self.test_automaton = self.load_automaton("../resources/state_machine01.txt")
         self.assertTrue(self.test_automaton)
+
+    # test to check initial
+    def test_initial_only(self) -> None:
+        self.test_automaton = self.load_automaton("../resources/state_machine01.txt")
+        for state in self.test_automaton.states:
+            if state.is_final and state.is_initial:
+                self.NotassertEqual("q0", state.state_id)
+            if state.is_initial and not state.is_final:
+                self.assertEqual("q0", state.state_id)
+            if state.is_final and not state.is_initial:
+                self.assertEqual("q2", state.state_id)
+            if not state.is_final and not state.is_initial:
+                self.assertEqual("q1", state.state_id)
+
+    # test to check final
+    def test_final_only(self) -> None:
+        self.test_automaton = self.load_automaton("../resources/state_machine01.txt")
+
+
+    # test to check both initial and final
+    def test_final_only(self) -> None:
+        self.test_automaton = serialize_automaton(self.load_automaton("../resources/state_machine01.txt"))
+
+    # test to check both initial and final
+    def test_write_file(self) -> None:
+        self.test_automaton = save_str_to_file('automata.txt',serialize_automaton(self.load_automaton("../resources/state_machine01.txt")))
 
     @staticmethod
     def load_automaton(file) -> Automaton:
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,47 +1,44 @@
+import re
+from os import linesep
 from automata.state_machine import Automaton, State
 
 
 def deserialize_automaton(input_file_path: str) -> Automaton:
     states = []
-    initial_state = State()
-    file = open(input_file_path, "r")
-    initial = False
-    final = False
-    for line in file:
-        if line.startswith("#") or line.strip() == '':
-            continue
-        curated_line = line.strip('\n').split("|")
-        sInit = curated_line[0]
-        transition = curated_line[1]
-        s_out = curated_line[2]
-        state = None
+    visited_state = set()
+    initial_state = None
+    final_state = None
 
-        # initial and acceptor
-        if ">*" in sInit:
-            state = State(sInit.strip(">*"), is_initial=True, is_final=True)
-            initial_state = state
-            initial = True
-            final = True
-        # initial
-        if ">" in sInit:
-            state = State(sInit.strip(">"), is_initial=True)
-            initial_state = state
-            initial = True
-        # acceptor
-        if "*" in sInit:
-            state = State(sInit.strip("*"), is_final=True)
-            final = True
-        # state has not been assigned yet, so that means
-        # that it is a "regular" state.
-        if state is None:
-            state = State(sInit)
+    with open(input_file_path, "r") as file:
 
-        states.append(state)
-        state.transitions[transition] = s_out
+        for line in file:
+            if line.startswith("#") or line.strip() == '':
+                continue
+            curated_line = line.strip('\n').split("|")
+            sInit = curated_line[0]
+            transition = curated_line[1]
+            s_out = curated_line[2]
+            initial = ">" in sInit
+            final = "*" in sInit
 
-    file.close()
+            stateId = re.sub("[>*]", "", sInit)
 
-    if not initial and not final:
+            if initial:
+                initial_state = stateId
+            if final:
+                final_state = stateId  # at least one final state
+
+            if stateId not in visited_state:
+                visited_state.add(stateId)
+                state = State(stateId, is_initial=initial, is_final=final)
+                state.transitions[transition] = [s_out]
+                states.append(StateType(stateId, state))
+            else:
+                for s in states:
+                    if s.stateId == stateId:
+                        s.add_transition(transition, s_out)
+
+    if initial_state is None and final_state is None:
         raise ValueError("The provided automata contains no initial or final state")
 
     return Automaton(initial_state, states)
@@ -49,9 +46,38 @@ def deserialize_automaton(input_file_path: str) -> Automaton:
 
 def serialize_automaton(input: Automaton) -> str:
     # TODO: Implement me!
-    pass
+    automaton = "# Generated string"
+    stats = input.states
+    state_list = []
+
+    for st in stats:
+        stat = st.state
+        for val in stat.transitions.items():
+            for v in val[1]:
+                if stat.is_initial:
+                    automaton += _calculate_str(">", stat.state_id + "|" + val[0] + "|" + v)
+                if not stat.is_initial and not stat.is_final:
+                    automaton += _calculate_str("", stat.state_id + "|" + val[0] + "|" + v)
+                if stat.is_final:
+                    automaton += _calculate_str("*", stat.state_id + "|" + val[0] + "|" + v)
+
+    #print(automaton)
+    return automaton
+
+
+def _calculate_str(symbol, state):
+    return linesep + symbol + state
 
 
 def save_str_to_file(output_file_path: str, contents: str):
-    # TODO: Implement me!
-    pass
+    with open(output_file_path, mode="w", encoding="utf8") as fl:
+        print(contents, file=fl)
+
+
+class StateType:
+    def __init__(self, stateId: str, state: State):
+        self.stateId = stateId
+        self.state = state
+
+    def add_transition(self, transition, s_out):
+        self.state.transitions[transition].append(s_out)
