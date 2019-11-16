f4d040d5fe9c64d7c533eb4af075eb6dea3e8cd1
C Hdz <47310270+charlieitesm@users.noreply.github.com>
2019-11-16 14:04:36 -0600

    Issue26-Add definition of natural order for State objects
    This will allow us to order lists of states using the builtin sorting
    functions and use the comparing operators.

--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -40,6 +40,48 @@ class AutomatonTest(unittest.TestCase):
 
         return Automaton(states)
 
+    def test_comparing_of_states(self):
+        s1 = State("1", is_initial=True)
+        s2 = State("2")
+        s3 = State("3")
+        s4 = State("4", is_final=True)
+        s5 = State("5", is_final=True)
+
+        self.assertFalse(s1 == s2)
+        clone = State("1", is_initial=True)
+
+        # Even though they are different objects, they represent the same state
+        self.assertFalse(s1 is clone)
+        self.assertTrue(s1 == clone)
+
+        self.assertTrue(s1 < s2)
+        self.assertTrue(s2 > s1)
+        self.assertTrue(s2 < s3)
+        self.assertTrue(s3 > s2)
+        self.assertTrue(s3 < s4)
+        self.assertTrue(s4 > s3)
+        self.assertTrue(s1 < s4)
+        self.assertFalse(s1 > s4)
+        self.assertTrue(s4 < s5)
+        self.assertFalse(s4 > s5)
+
+    def test_ordering_of_states(self):
+        s1 = State("1", is_initial=True, is_final=True)
+        s2 = State("2")
+        s3 = State("3")
+        s4 = State("4", is_final=True)
+        s5 = State("5", is_final=True)
+
+        states = [s5, s4, s3, s2, s1]
+        ordered = sorted(states)
+
+        self.assertIsNotNone(ordered)
+        self.assertTrue(ordered[0] is s1)
+        self.assertTrue(ordered[1] is s2)
+        self.assertTrue(ordered[2] is s3)
+        self.assertTrue(ordered[3] is s4)
+        self.assertTrue(ordered[4] is s5)
+
 
 if __name__ == '__main__':
     unittest.main()
